1e84e2bfbe3e9b8d934c6811c939da2259395333
Silvio Heinze <mail@silvioheinze.eu>
2021-03-09 12:18:36 +0000

    add app places

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     # Local apps
     'accounts',
     'pages',
+    'places',
 ]
 
 MIDDLEWARE = [
---
+++ places/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+
+class place(models.Model):
+    pass
+    
